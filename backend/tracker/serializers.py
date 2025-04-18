401ffd3ce40fee9369aefd2172613e76ffcf07be
ZhanarysRyskali <z_ryskali@kbtu.kz>
2025-04-18 17:51:33 +0600

    BackEndUrls

--- backend/tracker/serializers.py
+++ backend/tracker/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Category, Debt, Transaction, Goal
+from django.contrib.auth.models import User
 
 class CategorySerializerManual(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
@@ -24,3 +25,19 @@ class GoalSerializer(serializers.ModelSerializer):
         model = Goal
         fields = '__all__'
         read_only_fields = ['user', 'created_at']
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
+        )
+        return user
--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -1,16 +1,15 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import *
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('register/', register_view, name='register'), 
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'), 
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
+    path('logout/', logout_view, name='logout'), 
 
     path('categories/', category_list, name='category-list'),
-
     path('debts/', debt_list_create, name='debt-list-create'),
-
     path('transactions/', TransactionListCreate.as_view(), name='transaction-list-create'),
-
     path('goals/', GoalCRUD.as_view(), name='goal-crud'),
 ]
