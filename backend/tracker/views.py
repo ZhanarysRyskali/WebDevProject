4f999be56d0c7eb48f217b5ecdf66d6cb8160b2d
JohnuRyskali <139232971+JohnuRyskali@users.noreply.github.com>
2025-04-18 18:56:03 +0600

    Update views.py

--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -1,12 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view, permission_classes
 from .models import Transaction, Goal, Category, Debt
-from .serializers import TransactionSerializer, GoalSerializer, CategorySerializerManual, DebtSerializerManual
+from .serializers import TransactionSerializer, GoalSerializer, CategorySerializerManual, DebtSerializerManual, RegisterSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 class TransactionListCreate(APIView):
     permission_classes = [IsAuthenticated]
@@ -61,15 +63,21 @@ def debt_list_create(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class CustomLoginView(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({'token': token.key, 'user_id': token.user_id})
-
-
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def logout_view(request):
     request.user.auth_token.delete()
     return Response({'message': 'Logged out successfully'})
+
+@api_view(['POST'])
+def register_view(request):
+    serializer = RegisterSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+            'username': user.username
+        }, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
