e433111a1f88d094d3d12c38e036b3f07b95fdc9
ZhanarysRyskali <z_ryskali@kbtu.kz>
2025-04-23 22:10:29 +0600

    Added Button Transactions to Goals

--- frontend/src/app/pages/goals-page/goals-page.component.ts
+++ frontend/src/app/pages/goals-page/goals-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, ViewChild } from '@angular/core';
 import { GoalFormComponent } from '../../components/goal-form/goal-form.component';
 import { GoalListComponent } from '../../components/goal-list/goal-list.component';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-goals-page',
@@ -10,6 +12,12 @@ import { GoalListComponent } from '../../components/goal-list/goal-list.componen
 })
 export class GoalsPageComponent {
   @ViewChild(GoalListComponent) goalListComponent!: GoalListComponent;
+
+  constructor(private auth: AuthService, private router: Router) {}
+
+  onToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
 
   onGoalAdded(): void {
     this.goalListComponent.loadGoals();
